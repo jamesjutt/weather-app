1a4f327248040561cbbcb27392a7140a71e257b0
jamesjutt <jamesrjutt@gmail.com>
2019-07-16 18:21:40 -0400

    fix data output

--- public/js/app.js
+++ public/js/app.js
@@ -3,19 +3,32 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
+const createP = (message, className) => {
+  const tag = document.createElement("p");
+  tag.className = className || "";
+  const text = document.createTextNode(message);
+  tag.appendChild(text);
+  return tag;
+};
+
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`/weather?address=${location}`).then(response => {
-    response.json().then(data => {
+  fetch(`/weather?address=${location}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
       if (data.error) {
         messageOne.textContent = data.error;
       } else {
+        console.log(data);
         messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecast;
+        for (const key in data.forecast) {
+          messageTwo.appendChild(createP(`${key}: ${data.forecast[key]}`));
+        }
       }
     });
-  });
 });
